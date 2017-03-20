abff1c560e81a226d3ecd755b5e734fc0bacaf2c
geode76 <ahuddleston80@yahoo.com>
2017-03-19 19:12:33 -0700

    dinner

--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -21,7 +21,7 @@ var routes = function(Book){
             {
                 query.genre = req.query.genre;
             }
-            Book.find(query, function(err,books){
+            Book.find(query, function(err, books){
                 if(err)
                     res.status(500).send(err);
                 else
@@ -29,17 +29,45 @@ var routes = function(Book){
             });
         });
 
+
+    bookRouter.use('/:bookId', function(req, res, next){
+        Book.findById(req.params.bookId, function(err, book){
+            if(err)
+                res.status(500).send(err);
+            else if(book)
+            {
+                req.book = book;
+                next();
+            }
+            else
+            {
+                res.status(404).send('No Book Found!');
+            }
+        });
+    });
     bookRouter.route('/:bookId')
-        .get(function(req,res){
+        .get(function(req, res){
 
+            res.json(req.book);
 
-            Book.findById(req.params.bookId, function(err,book){
+        })
+        .put(function(req, res){
+            req.book.title = req.body.title;
+            req.book.author = req.body.author;
+            req.book.genre = req.body.genre;
+            req.book.read = req.body.read;
+            req.book.save(function(err){
                 if(err)
                     res.status(500).send(err);
-                else
-                    res.json(book);
+                else{
+                    res.json(req.book);
+                }
             });
+        })
+        .patch(function(req,res){
+
         });
+
     return bookRouter;
 };
 
