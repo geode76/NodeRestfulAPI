a67022f4513a8c1b8b277ab754344c6547ebf1d9
geode76 <ahuddleston80@yahoo.com>
2017-03-24 08:56:59 -0700

    final version

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -1,18 +1,34 @@
 var bookController = function(Book){
 
+    var findByIdInterceptor = function(req, res, next){
+        Book.findById(req.params.bookId, function(err,book){
+            if(err)
+                res.status(500).send(err);
+            else if(book)
+            {
+                req.book = book;
+                next();
+            }
+            else
+            {
+                res.status(404).send('No Book Found');
+            }
+        });
+    }
+
     var post = function(req, res){
         var book = new Book(req.body);
 
         if(!req.body.title){
             res.status(400);
-            res.send('Title is required');
+            res.send('Title is Required');
         }
         else {
             book.save();
             res.status(201);
             res.send(book);
         }
-    }
+    };
 
     var get = function(req, res){
 
@@ -28,11 +44,64 @@ var bookController = function(Book){
             else
                 res.json(books);
         });
-    }
+    };
+
+    var findById = function(req, res){
+
+            res.json(req.book);
+
+        }
+
+    var update = function(req, res){
+            req.book.title = req.body.title;
+            req.book.author = req.body.author;
+            req.book.genre = req.body.genre;
+            req.book.read = req.body.read;
+            req.book.save(function(err){
+                if(err)
+                    res.status(500).send(err);
+                else{
+                    res.json(req.book);
+                }
+            });
+        }
+
+    var patch = function(req, res){
+            if(req.body._id)
+                delete req.body._id;
+
+            for(var p in req.body)
+            {
+                req.book[p] = req.body[p];
+            }
+
+            req.book.save(function(err){
+                if(err)
+                    res.status(500).send(err);
+                else{
+                    res.json(req.book);
+                }
+            });
+        }
+
+    var remove = function(req, res){
+            req.book.remove(function(err){
+                if(err)
+                    res.status(500).send(err);
+                else{
+                    res.status(204).send('Removed');
+                }
+            });
+        }
 
     return {
         post: post,
-        get: get
+        get: get,
+        findById: findById,
+        update: update, 
+        patch: patch,
+        remove: remove,
+        findByIdInterceptor: findByIdInterceptor
     }
 }
 
